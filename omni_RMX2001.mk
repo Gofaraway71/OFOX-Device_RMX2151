ce95c36197010fa7c44369e2d64e4c299fa18d7e
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2021-08-24 08:47:32 +0100

    Update omni_RMX2001.mk

--- omni_RMX2001.mk
+++ omni_RMX2001.mk
@@ -16,7 +16,12 @@
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
+# Inherit from our PitchBlack configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX2001
